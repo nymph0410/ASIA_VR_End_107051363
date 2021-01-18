3e19c63af5ba62688ca8903e12b2acdc330188e0
nymph0410 <71680170+nymph0410@users.noreply.github.com>
2021-01-19 07:17:13 +0800

    互動

---
+++ VR_End_107051363/Assets/材質/Scripts/ScoreManger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManger : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text testScore;
+    [Header("分數")]
+    public Text score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.name);
+    }
+}
