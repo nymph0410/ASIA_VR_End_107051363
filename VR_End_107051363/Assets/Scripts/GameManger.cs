22cc12dcc6f39e482f909a2420a2e54eaf499536
nymph0410 <71680170+nymph0410@users.noreply.github.com>
2021-01-19 11:42:06 +0800

    END

---
+++ VR_End_107051363/Assets/Scripts/GameManger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManger : MonoBehaviour
+{
+    public void RestartGame() 
+    {
+        SceneManager.LoadScene("籃球場");
+    
+    }
+
+    public void QuitGame() 
+    {
+        Application.Quit();
+    }
+  
+}
---
+++ VR_End_107051363/Assets/Scripts/ScoreManger.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManger : MonoBehaviour
+{
+    [Header("分數介面")]
+    public Text textScore;
+    [Header("分數")]
+    public int score;
+    [Header("投進的分數")]
+    public int scoreIn = 2;
+    [Header("進球音效")]
+    public AudioClip soundIn;
+
+    private AudioSource aud;
+
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag =="眼睛"&& other.transform.position.y>2.5f)
+        {
+            AddScore();
+        }
+        if (other.transform.root.name == "Player")
+        {
+            scoreIn = 3;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.root.name == "Player" )
+        {
+            scoreIn = 2;
+
+        }
+    }
+
+    private void AddScore() 
+    {
+        score += scoreIn;
+        textScore.text = "分數：" + score;
+        aud.PlayOneShot(soundIn,Random.Range(1f,2f));
+    
+    }
+}
